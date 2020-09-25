9d8cae4386b49fe8768bf045b50d015b188c7094
heffoo <57847291+heffoo@users.noreply.github.com>
2020-09-25 17:13:33 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Appl from "./components /nav";
+
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Appl
+      items={[
+        {
+          to: '/first',
+          name: "first item",
+        },
+        {
+          to: '/second',
+          name: "second item",
+        },
+        {
+          to: '/third',
+          name: "third item",
+        },
+      ]}
+    />
   );
 }
-
 export default App;
---
+++ src/components /pages/home/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './home.scss'
+
+
+export default function First() {
+    return <div className="page">
+      <div className="mainPage">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, ad?
+      </div>
+      </div>
+  }
